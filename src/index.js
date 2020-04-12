7c4d85141a8576683f95a11fea7d172986683e6a
jacobliu <441701328@qq.com>
2020-04-12 14:09:34 +0800

    fix the code style

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import Table from '../src/components/table/Table';
 
 
 const install = function (Vue) {
-    Vue.component(Table.name, Table);
+  Vue.component(Table.name, Table);
 };
 
 /* istanbul ignore if */
 if (typeof window !== 'undefined' && window.Vue) {
-    install(window.Vue);
+  install(window.Vue);
 }
 
 module.export = {
-    install,
-    Table,
-}
+  install,
+  Table,
+};
